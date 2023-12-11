59396ca007e1fbf6b825ade67f16689b667546f4
jacbar02 <jacobbartlett555@gmail.com>
2023-12-11 15:44:51 -0600

    at this point, im completely confused

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-       /*Setup any variables you need and call your methods
-       * here in the main method */
+        double inputData = inputPurchasePrice();
+        double stateTax = calculateTax(inputData, .04);
+        double countyTax = calculateTax(inputData, .02);
+        double totalSalesTax = calculateTotal(stateTax, countyTax);
+        double totalSale = calculateTotal(inputData, totalSalesTax);
+        displaysTotals(inputData, stateTax, countyTax, totalSalesTax, totalSale);
     }
 
-    /* Write a method called inputPurchasePrice that
-    *  takes the user input for price, converts to
-    *  a double, and returns the price
-    * */
+    public static double inputPurchasePrice() {
+        double purchasePrice;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter purchase price: ");
+        purchasePrice = scanner.nextDouble();
+        return purchasePrice;
+    }
 
-    /* Write a method called calculateTax that
-     * accepts price and tax rate as parameters and
-     * calculates and returns the tax
-     * */
+    public static double calculateTax(double inputdata, double taxrate){
+        return inputdata * taxrate;
+    }
 
-    /* Write a method called calculateTotal that
-     * accepts two double values nd calculates and
-     * returns the total
-     * */
+    public static double calculateTotal(double numberone, double numbertwo){
+        return numberone + numbertwo;
+    }
 
-    /* Write a method called displayTotals that takes
-    *  purchasePrice, stateTax, countyTax, totalTax and totalPrice
-    *  as parameters. Output using decimal formatter amd printf */
-
+    public static void displaysTotals(double inputData, double stateTax, double countyTax, double totalSalesTax, double totalSale) {
+        System.out.printf("Purchase Price: $%.2f\n", inputData);
+        System.out.printf("State Tax (4%%): $%.2f\n", stateTax);
+        System.out.printf("County Tax (2%%): $%.2f\n", countyTax);
+        System.out.printf("Total Sales Tax: $%.2f\n", totalSalesTax);
+        System.out.printf("Total Sale: $%.2f\n", totalSale);
+    }
 }
+
